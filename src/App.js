0c43cf11ef38e6821babfc326f8da8320712b33a
Bhavesh S Singh <bhaveshsinghbiz@gmail.com>
2021-02-04 18:24:13 +0530

    building navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import SearchMovie from "./search/SearchMovie";
+// import NavBar from "./shared/NavBar";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
---
+++ src/shared/NavBar.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import * as FaIcons from "react-icons/fa";
+
+// const NavBar = () => {
+//   return (
+//     <>
+//       <div className="navbar">
+//         <Link to="#">
+//           <FaIcons.FaBars />
+//         </Link>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default NavBar;
