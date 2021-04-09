3d9750e0459497b338dc15daa27259bc50f70f90
Bhavesh S Singh <bhaveshsinghbiz@gmail.com>
2021-04-09 12:24:54 +0530

    style - scrollbar,movie container

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../../components/navbar/Footer";
 import "./search.css";
 import SearchComponent from "./SearchComponent";
 
@@ -6,6 +7,7 @@ const Search = () => {
   return (
     <>
       <SearchComponent />
+      <Footer />
     </>
   );
 };
